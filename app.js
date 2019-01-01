e5ff6575cfd12f69edb773c23aa81876ce4ad155
Christopher Caldarella <christopher.caldarella@outlook.com>
2019-01-01 14:39:03 -0700

    Add Error Code to error.html using app.js

--- app.js
+++ app.js
@@ -66,3 +66,26 @@ function openMenu(item, content) {
 	}
 }
 
+
+function qs(search_for) {
+	var query = window.location.search.substring(1);
+	var parms = query.split('&');
+	
+	for(var i = 0; i < parms.length; i++)
+	{
+		var pos = parms[i].indexOf('=');
+		if(pos > 0 && search_for == parms[i].substring(0, pos) ) {
+			return parms[i].substring(pos+1);
+		}
+	}
+	return "";
+}
+
+function loadError() {
+	var errorCode = qs("error");
+	if (errorCode != "")
+	{ document.getElementById("show-error-code").innerHTML = "(" + qs("error") + " Error)"; }
+}
+
+
+
